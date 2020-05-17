212ec72d4265dc1c7c0d18c859647d8eed808243
Navjeet Singh <navjeet.singh5@yahoo.com>
2020-05-17 22:49:13 +0700

    commiting project

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import DIRECTORY_DATA from './directory.data.js';
+import MenuItem from '../menu-item/menu-item.component';
+
+import './directory.styles.scss';
+
+const Directory = () => (
+  <div className='directory-menu'>
+    {DIRECTORY_DATA.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>
+);
+
+export default Directory;
